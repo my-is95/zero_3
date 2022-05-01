7c4157cf97da3de14b8dea0a3fa1a93dac6582eb
my-is95 <miyoshi.exjge@gmail.com>
2022-05-01 21:19:56 +0900

    [change]不要となったTestコードの削除

--- mycode/main.py
+++ mycode/main.py
@@ -283,22 +283,6 @@ def numerical_diff(f, x, eps=1e-4):
     return (y1.data - y0.data) / (2 * eps)
 
 
-class SquareTest(unittest.TestCase):
-    def test_forward(self):
-        x = Variable(np.array(2.0))
-        y = square(x)
-        expected = np.array(4.0)
-        self.assertEqual(y.data, expected)
-
-    def test_gradient_check(self):
-        x = Variable(np.random.rand(1)) # ランダムな入力値を生成
-        y = square(x)
-        y.backward()
-        num_grad = numerical_diff(square, x)
-        flg = np.allclose(x.grad, num_grad)
-        self.assertTrue(flg)
-
-
 def main():
     x = Variable(np.array([1.0]))
     y = np.array([2.0]) + x
