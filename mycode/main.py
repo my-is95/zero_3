9102a9bb086b5a4c21caf04f284101369ce871bb
my-is95 <miyoshi.exjge@gmail.com>
2022-05-01 21:18:29 +0900

    [change]Step22 演算子のオーバーロード(3) 負数、引き算、割り算、累乗の演算子

--- mycode/main.py
+++ mycode/main.py
@@ -143,6 +143,7 @@ class Function:
         raise NotImplementedError()
 
 
+# 足し算
 class Add(Function):
     def forward(self, x0, x1):
         x1 = as_array(x1)
@@ -152,6 +153,7 @@ class Add(Function):
     def backward(self, gy):
         return gy, gy
 
+# 掛算
 class Mul(Function):
     def forward(self, x0, x1):
         x1 = as_array(x1)
@@ -161,6 +163,50 @@ class Mul(Function):
     def backward(self, gy):
         x0, x1 = self.inputs[0].data, self.inputs[1].data
         return gy * x1, gy * x0
+
+# 負数
+class Neg(Function):
+    def forward(self, x):
+        return -x
+
+    def backward(self, gy):
+        return -gy
+
+# 引き算
+class Sub(Function):
+    def forward(self, x0, x1):
+        y = x0 - x1
+        return y
+
+    def backward(self, gy):
+        return gy, -gy
+
+# 割り算
+class Div(Function):
+    def forward(self, x0, x1):
+        y = x0 / x1
+        return y
+
+    def backward(self, gy):
+        x0, x1 = self.inputs[0].data, self.inputs[1].data
+        gx0 = gy / x1
+        gx1 = gy * (-x0 / x1 ** 2)
+        return gx0, gx1
+
+# べき乗
+class Pow(Function):
+    def __init__(self, c):
+        self.c = c
+
+    def forward(self, x):
+        y = x ** self.c
+        return y
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        c = self.c
+        gx = c * x ** (self.c - 1) * gy
+        return gx
 
 class Square(Function):
     def forward(self, x):
@@ -189,6 +235,27 @@ def add(x0, x1):
 def mul(x0, x1):
     return Mul()(x0, x1)
 
+def neg(x):
+    return Neg()(x)
+
+def sub(x0, x1):
+    return Sub()(x0, x1)
+
+def rsub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x1, x0)
+
+def div(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x0, x1)
+
+def rdiv(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x1, x0)    # x1, x0 を入れ替え
+
+def pow(x, c):
+    return Pow(c)(x)
+
 def square(x):
     return Square()(x)
 
@@ -200,6 +267,12 @@ Variable.__mul__ = mul
 Variable.__rmul__ = mul # 可換のためadd関数の再利用が可能
 Variable.__add__ = add
 Variable.__radd__ = add # 可換のためadd関数の再利用が可能
+Variable.__neg__ = neg
+Variable.__sub__ = sub
+Variable.__rsub__ = rsub
+Variable.__div__ = div
+Variable.__rdiv__ = rdiv
+Variable.__pow__ = pow
 
 # 中心差分近似によって、数値微分を求める
 def numerical_diff(f, x, eps=1e-4):
