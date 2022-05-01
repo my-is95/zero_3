441eace98e00348dcd1e062ccccebf5f8c59a251
my-is95 <miyoshi.exjge@gmail.com>
2022-05-01 17:06:27 +0900

    [change]step19
    メモリ使用量を減らすモード
    推論時に逆伝播を行わないことを活かし、保持するメンバ変数を減らす

--- mycode/main.py
+++ mycode/main.py
@@ -1,6 +1,23 @@
 import numpy as np
 import weakref
+import contextlib
 import unittest
+
+
+class Config:
+    enable_backprop = True  # 逆伝播を可能にするかどうか
+
+@contextlib.contextmanager
+def using_config(name, value):
+    old_value = getattr(Config, name)
+    setattr(Config, name, value)
+    try:
+        yield
+    finally:
+        setattr(Config, name, old_value)
+
+def no_grad():
+    return using_config('enable_backprop', False)
 
 
 class Variable:
@@ -21,7 +38,7 @@ class Variable:
     def cleargrad(self):
         self.grad = None
 
-    def backward(self):
+    def backward(self, retain_grad=False):
         if self.grad is None:
             self.grad = np.ones_like(self.data)
 
@@ -51,6 +68,10 @@ class Variable:
                 if x.creator is not None:
                     add_func(x.creator)
 
+            if not retain_grad:
+                for y in f.outputs:
+                    y().grad = None # 参照カウントが0になり、微分のデータはメモリから消去される
+
 
 def as_array(x):
     if np.isscalar(x):
@@ -71,12 +92,13 @@ class Function:
         if not isinstance(ys, tuple):   # タプルではない場合の追加対応
             ys = (ys,)
         outputs = [Variable(as_array(y)) for y in ys]
-        # 逆伝播を正常な順番で行うために世代管理
-        self.generation = max([x.generation for x in inputs])
-        for output in outputs:
-            output.set_creator(self)
-        self.inputs = inputs
-        self.outputs = [weakref.ref(output) for output in outputs]
+        if Config.enable_backprop:
+            # 逆伝播を正常な順番で行うために世代管理
+            self.generation = max([x.generation for x in inputs])
+            for output in outputs:
+                output.set_creator(self)
+            self.inputs = inputs
+            self.outputs = [weakref.ref(output) for output in outputs]
         # 関数の戻り値が1つの場合は、その1つの変数を直接返すようにしている
         return outputs if len(outputs) > 1 else outputs[0]
 
@@ -155,13 +177,10 @@ class SquareTest(unittest.TestCase):
 
 
 def main():
-    x = Variable(np.array(2.0))
-    a = square(x)
-    y = add(square(a), square(a))
-    y.backward()
-
+    with no_grad():
+        x = Variable(np.array(2.0))
+        y = square(x)
     print(y.data)
-    print(x.grad)
 
 
 if __name__ == "__main__":
