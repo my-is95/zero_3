7a63aba97e7e2caca40d817e6c38d75487e42588
my-is95 <miyoshi.exjge@gmail.com>
2022-05-05 18:17:47 +0900

    [change]Step43 非線形な回帰を推論するための簡易なニューラルネットの実装

--- mycode/dezero/functions.py
+++ mycode/dezero/functions.py
@@ -3,6 +3,7 @@ import numpy as np
 from dezero import utils
 from dezero.core import Function
 from dezero.core import as_variable
+from dezero.core import as_array
 
 class Sin(Function):
     def forward(self, x):
@@ -107,6 +108,17 @@ class MatMul(Function):
         gW = matmul(x.T, gy)
         return gx, gW
 
+class Exp(Function):
+    def forward(self, x):
+        x = as_array(x)
+        y = np.exp(x)
+        return y
+
+    def backward(self, gy):
+        y = self.outputs[0]()  # weakref
+        gx = y * gy
+        return gx
+
 class MeanSquaredError(Function):
     def forward(self, x0, x1):
         diff = x0 - x1
@@ -120,6 +132,20 @@ class MeanSquaredError(Function):
         gx0 = gy * diff * (2. / len(diff))
         gx1 = -gx0
         return gx0, gx1
+
+class Linear(Function):
+    def forward(self, x, W, b):
+        y = x.dot(W)
+        if b is not None:
+            y += b
+        return y
+
+    def backward(self, gy):
+        x, W, b = self.inputs
+        gb = None if b.data is None else sum_to(gy, b.shape)
+        gx = matmul(gy, W.T)
+        gW = matmul(x.T, gy)
+        return gx, gW, gb
 
 
 def sin(x):
@@ -155,5 +181,26 @@ def sum_to(x, shape):
 def matmul(x, W):
     return MatMul()(x, W)
 
+def exp(x):
+    return Exp()(x)
+
 def mean_squared_error(x0, x1):
     return MeanSquaredError()(x0, x1)
+
+def linear(x, W, b=None):
+    return Linear()(x, W, b)
+
+
+def linear_simple(x, W, b=None):
+    x, W = as_variable(x), as_variable(W)
+    t = matmul(x, W)
+    if b is None:
+        return t
+    y = t + b
+    t.data = None   # 逆伝播において計算に使用されないためメモリを解放する
+    return y
+
+def sigmoid_simple(x):
+    x = as_variable(x)
+    y = 1 / (1 + exp(-x))
+    return y
